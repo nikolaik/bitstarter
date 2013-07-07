4a5877806c2931b59f5709a48aad5050d19c1e23
Nikolai R Kristiansen <nikolaik@gmail.com>
2013-07-07 15:00:29 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var filename = 'index.html';
+  var buf = new Buffer(fs.readFileSync(filename));
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
